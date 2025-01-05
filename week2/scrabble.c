71c1d9d97c67042921eb3edec3e77fe2022c5664
Emad Masandeh <98871381+emasandeh@users.noreply.github.com>
2025-01-05 13:28:27 +0300

    Update scrabble.c

--- week2/scrabble.c
+++ week2/scrabble.c
@@ -1,1 +1,52 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string player1 = get_string("Player 1: ");
+    string player2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(player1);
+    int score2 = compute_score(player2);
+
+    // Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score2 > score1)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
+}
+
+int compute_score(string word)
+{
+    int score = 0;
+
+    // Compute the score for the word
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        if (isalpha(word[i]))
+        {
+            // Convert letter to uppercase and calculate its score
+            int index = toupper(word[i]) - 'A';
+            score += POINTS[index];
+        }
+    }
+
+    return score;
+}
